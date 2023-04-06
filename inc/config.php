34aede89f59c82ecd918da8ba1f7a4ff49809f99
Kevin Gibbs <kevin@Kevingibbs.com>
2023-04-06 15:01:08 -0500

    initial commit

---
+++ inc/config.php
@@ -0,0 +1,5 @@
+<?php
+define("DB_HOST", getenv('envDbserver'));
+define("DB_USERNAME", getenv('envDbuser'));
+define("DB_PASSWORD", getenv('envDbpass'));
+define("DB_DATABASE_NAME", getenv('envDbnameRestAPI'));
